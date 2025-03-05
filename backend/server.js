c17af748d31fba7c49ab9d0614116e1396db0d25
nipunbakshi12 <156699344+nipunbakshi12@users.noreply.github.com>
2025-03-05 15:14:08 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB()
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     // allowedHeaders: ['Content-Type', 'Authorization']
 }));
